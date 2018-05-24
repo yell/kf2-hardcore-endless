2d9cca9e650e3c9fb3df9079b27ba3c642c661a8
monsta <yell.bondarenko@gmail.com>
2018-05-24 10:40:29 +0200

    minor cleanup

--- custom_waves_generator.py
+++ custom_waves_generator.py
@@ -53,8 +53,9 @@ Z = KF2_ZED
 
 
 class KF2_EndlessUtility(object):
-    """Utility functions to compute zed count, zed count multipliers, 
-    spawn rates etc. in KF2 Endless mode. Numbers as per 1.5.2018
+    """Utility class with routines to compute zed count,
+    zed count multipliers, spawn rates etc. in KF2 Endless mode.
+    Numbers as per 01.05.2018
 
     References
     ----------
@@ -74,21 +75,31 @@ class KF2_EndlessUtility(object):
         return num_wave % 5 == 0
 
     @staticmethod
-    def _base_number_of_zeds(num_wave):
+    def _base_zeds_count(num_wave):
         if KF2_EndlessUtility.is_boss_wave(num_wave):
             return 0
-        if num_wave < 10:
-            return {1: 25, 2: 28, 3: 32, 4: 32, 6: 35, 7: 35, 8: 35, 9: 40}[num_wave]
-        return 42 #  `num_wave` >= 11
+        return {1: 25,
+                2: 28,
+                3: 32,
+                4: 32,
+                6: 35,
+                7: 35,
+                8: 35,
+                9: 40}.get(num_wave, 42)
 
     @staticmethod
-    def base_number_of_zeds(num_wave):
+    def base_zeds_count(num_wave):
         assert KF2_EndlessUtility.is_valid_num_wave(num_wave)
-        return KF2_EndlessUtility._base_number_of_zeds(num_wave)
+        return KF2_EndlessUtility._base_zeds_count(num_wave)
 
     @staticmethod
     def _wave_length_modifier(n_players):
-        return {1: 1, 2: 2, 3: 2.75, 4: 3.5, 5: 4, 6: 5}[n_players]
+        return {1: 1.0,
+                2: 2.0,
+                3: 2.75,
+                4: 3.5,
+                5: 4.0,
+                6: 5.0}[n_players]
 
     @staticmethod
     def wave_length_modifier(n_players):
@@ -98,7 +109,11 @@ class KF2_EndlessUtility(object):
     @staticmethod
     def _wave_count_mod_hoe(num_wave):
         if num_wave <= 25:
-            return {0: 0.85, 1: 0.9, 2: 0.95, 3: 1., 4: 1.05}[(num_wave - 1)/5]
+            return {0: 0.85,
+                    1: 0.9,
+                    2: 0.95,
+                    3: 1.0,
+                    4: 1.05}[(num_wave - 1)/5]
         return 1.15 + 0.1 * ((num_wave - 26)/5)
 
     @staticmethod
@@ -109,16 +124,19 @@ class KF2_EndlessUtility(object):
         
     @staticmethod
     def n_zeds(num_wave, n_players, difficulty='hoe'):
-        n = KF2_EndlessUtility.base_number_of_zeds(num_wave) *\
+        n = KF2_EndlessUtility.base_zeds_count(num_wave) *\
             KF2_EndlessUtility.wave_length_modifier(n_players) *\
             KF2_EndlessUtility.wave_count_mod(num_wave, difficulty)
         return int(n)
 
     @staticmethod
     def _spawn_rate_modifier_hoe(num_wave):
-        if num_wave <= 30:
-            return {0: 0.68, 1: 0.65, 2: 0.6, 3: 0.55, 4: 0.5, 5: 0.2}[(num_wave - 1)/5]
-        return 0.
+        return {0: 0.68,
+                1: 0.65,
+                2: 0.6,
+                3: 0.55,
+                4: 0.5,
+                5: 0.2}.get((num_wave - 1)/5, 0.)
 
     @staticmethod
     def spawn_delay(base_spawn_delay, num_wave):
